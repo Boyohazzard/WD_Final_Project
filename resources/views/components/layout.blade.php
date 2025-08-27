7b716b65e03f42f7104e5ba4534a081db2b96b0f
GlaabJ <jonglaab@gmail.com>
2025-08-27 18:36:44 -0400

    added simple navbar and blank views

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,13 +1,65 @@
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 <html>
   <head>
     <title>{{ $title ?? 'Final Project' }}</title>
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
   </head>
   <body>
     <nav>
       <h3>Welcome to product site, a place for all your product needs</h3>
       <hr>
     </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="{{ url('/') }}">MyShop</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="mainNavbar">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/') }}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/products') }}">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/cart') }}">Cart</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ url('/orders') }}">Orders</a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        @guest
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">Register</a>
+          </li>
+        @else
+          <li class="nav-item">
+            <span class="nav-link">Hi, {{ Auth::user()->name }}</span>
+          </li>
+          <li class="nav-item">
+            <form method="POST" action="{{ route('logout') }}">
+              @csrf
+              <button class="nav-link btn btn-link" type="submit">Logout</button>
+            </form>
+          </li>
+        @endguest
+      </ul>
+    </div>
+  </div>
+</nav>
+
     {{ $slot }}
     <footer>
       <hr />
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,40 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name("home");
 
-Route::get('/contact', function () {
+Route::get('contact', function () {
     return view('contact');
-});
+})->name("contact");
+
+Route::get('practice', function () {
+    return view("practice");
+})->name("practice");
+
+Route::get('login', function () {
+    return view('login');
+})->name("login");
+
+Route::get('logout', function () {
+    return view('logout');
+})->name("logout");
+
+Route::get('products', function () {
+    return view('products');
+})->name("products");
+
+Route::get('register', function () {
+    return view('register');
+})->name("register");
+
+Route::get('cart', function () {
+    return view('cart');
+})->name("cart");
+
+Route::get('orders', function () {
+    return view('orders');
+})->name("orders");
+
+//Route::post('product', function () {
+//    return view('product');
+//})->name("product");
