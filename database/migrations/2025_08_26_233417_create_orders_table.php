c7d7ded027e0a12891c2ac4390ffebb1b3bec9ae
sarah.niazalizadeh <sarah.niazalizadeh@gmail.com>
2025-08-26 20:05:37 -0400

    Fixed some errors in model relations

--- database/migrations/2025_08_26_233417_create_orders_table.php
+++ database/migrations/2025_08_26_233417_create_orders_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('total_cents');
+            $table->string('status')->default('pending');
         });
     }
 
--- database/migrations/2025_08_26_233501_create_order_items_table.php
+++ database/migrations/2025_08_26_233501_create_order_items_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('unit_price_cents');
+            $table->integer('quantity');
+            $table->integer('line_total_cents');
         });
     }
 
